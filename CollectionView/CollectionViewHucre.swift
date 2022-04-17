88365054cfade57b85542547dbdff07df0eb850a
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-04-17 12:32:52 +0300

    Collection View First Commit & Push

---
+++ CollectionView/CollectionViewHucre.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewHucre.swift
+//  CollectionView
+//
+//  Created by Ömer Faruk Kılıçaslan on 17.04.2022.
+//
+
+import UIKit
+
+class CollectionViewHucre: UICollectionViewCell {
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+}
